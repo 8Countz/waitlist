931060db9d3081e06697b045e97c47bf3a2ba1c8
Srinivasa Rao M <srinivasarao.m@everest.engineering>
2024-02-11 09:50:57 +0530

    show image first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,13 @@ const IndexPage = () => {
   return (
     <main>
       <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+        <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
+          <StaticImage
+            alt=""
+            src="https://images.unsplash.com/photo-1618260970135-8afc4074aad4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+        </div>
         <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
           <div className="mx-auto max-w-lg text-center">
             <h1 className="text-2xl font-bold sm:text-3xl">
@@ -52,14 +59,6 @@ const IndexPage = () => {
               </button>
             </div>
           </form>
-        </div>
-
-        <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
-          <StaticImage
-            alt=""
-            src="https://images.unsplash.com/photo-1618260970135-8afc4074aad4?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="absolute inset-0 h-full w-full object-cover"
-          />
         </div>
       </section>
     </main>
