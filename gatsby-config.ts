9e9633fd33f05a61f4c201524ce4013580f1878d
Srinivasa Rao M <srinivasarao.m@everest.engineering>
2025-03-23 12:26:10 +0530

    add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,20 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `8Countz Waitlist`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `8Countz`,
+    siteUrl: `https://8countz.com/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
